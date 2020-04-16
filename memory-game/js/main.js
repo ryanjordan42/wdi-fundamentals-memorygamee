a399850366d02439285495572d5dc5d90bd074b1
Ryan Burnett <ryanburnett@ryans-air.lan>
2020-04-16 16:56:00 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,18 @@
-console.log("Up and running!");
+	var cards = ["queen", "queen", "king", "king"]; 
+	var cardsInPlay = []; 
+	var cardOne = cards[0]; 
+	var cardTwo = cards[2]; 
+	
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+	cardsInPlay.push(cardOne); 
+	cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardOne);
+console.log("User flipped " + cardOne); 
+console.log("User flipped " + cardTwo); 
+	
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+	};
+	
